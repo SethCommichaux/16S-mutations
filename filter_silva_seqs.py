58305b446b2ae6dcf79f8ec1d8b21a4fce0b05fb
SethCommichaux <yofi01@gmail.com>
2025-04-09 14:54:16 -0400

    Update filter_silva_seqs.py

--- filter_silva_seqs.py
+++ filter_silva_seqs.py
@@ -1,3 +1,6 @@
+### Written by Seth Commichaux on April 9, 2025
+### Program counts the sequences in the SILVA 16S rRNA database and filters out eukaryotic sequences or those with ambiguous nucleotides.
+
 from Bio import SeqIO
 
 f = 'SILVA_138.2_SSURef_NR99_tax_silva.fasta'
@@ -8,9 +11,9 @@ with open('SILVA_138.2_SSURef_NR99_tax_silva_filtered.fasta','w') as out:
   for i in SeqIO.parse(f,'fasta'):
     seqs_before_filtering += 1
     d,s = str(i.description),str(i.seq).upper().replace('T','U')
-    if 'eukaryota' in d.lower():
+    if 'eukaryota' in d.lower(): # filters out eukaryotic sequences from mitochondria and plastids
       continue
-    elif set(s) != set('ACGU'):
+    elif set(s) != set('ACGU'): # filters out sequences with ambiguous nucleotides like N, Y, M, V, etc.
       continue
     else:
       out.write(f'>{d}\n{s}\n')
